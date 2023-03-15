ec4e1ea84ac6afb30140e30c6a8ffb54f04c86c8
Girraj Sharma <girrajsharma@Girrajs-MacBook-Air.local>
2023-03-15 18:10:20 +0530

    initial commit

---
+++ android/lib/build/generated/source/buildConfig/debug/com/marianhello/bgloc/react/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.marianhello.bgloc.react;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.marianhello.bgloc.react";
+  public static final String BUILD_TYPE = "debug";
+}
